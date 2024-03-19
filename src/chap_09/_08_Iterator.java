c5f41e219a1ea841bb6aacfdeda9065f93c86db1
D code <note37@naver.com>
2024-03-19 23:27:09 +0900

    2024-03-19

--- src/chap_09/_08_Iterator.java
+++ src/chap_09/_08_Iterator.java
@@ -1,9 +1,7 @@
 package chap_09;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-
+import java.util.*;
+import java.util.HashSet;
 public class _08_Iterator {
     public static void main(String[] args) {
         // 이터레이터
@@ -38,10 +36,38 @@ public class _08_Iterator {
                 it.remove(); // 삭제
             }
         }
-        it = list.iterator();
+        it = list.iterator(); // 커서를 처음 위치로 이동
         while (it.hasNext()){
             System.out.println(it.next());
         }
+        System.out.println("-----HashSet------");
+        HashSet<String> set = new HashSet<>();
+        set.add("유재석");
+        set.add("박명수");
+        Iterator<String> itSet = set.iterator(); // set.iterator(); 작성 후 ctrl alt v
+        while (itSet.hasNext()) {
+            System.out.println(itSet.next());
+        }
+        System.out.println("------HashMap Key--------");
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("유재석",10);
+        map.put("박명수",5);
+        // map.iterator() // key value 로 이루어져 사용 불가
+        Iterator itMapKey = map.keySet().iterator();
+        while (itMapKey.hasNext()) {
+            System.out.println(itMapKey.next());
+        }
+        System.out.println("------HashMap Value--------");
+        Iterator<Integer> itMapVal = map.values().iterator();
+        while (itMapVal.hasNext()) {
+            System.out.println(itMapVal.next());
+        }
+        System.out.println("------HashMap entry 순회--------");
+        Iterator<Map.Entry<String, Integer>> itEntry = map.entrySet().iterator();
+        while (itEntry.hasNext()) {
+            System.out.println(itEntry.next());
+        }
+
 
     }
 }
