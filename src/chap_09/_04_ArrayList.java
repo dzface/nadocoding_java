a3f560d830f89333154e65d8e7f8b3140806adb2
dzface <dzface031@gmail.com>
2024-10-16 23:36:31 +0900

    ArrayList 복습

---
+++ src/chap_09/_04_ArrayList.java
@@ -0,0 +1,38 @@
+package chap_09;
+
+import java.util.ArrayList;
+
+public class _04_ArrayList {
+    public static void main(String[] args) {
+        int[] array =new int[3];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+//      array[3] = 5; 배열은 처음 초기화한 크기이상으로 데이터를 넣을 수 없었음
+//      컬렉션 프레임워크
+//        List, Set, Map 크게 3가지가 있음
+        // 리스트에는 ArrayList, LinkedList가 있음
+        // ArrayList 선언
+        ArrayList<String> list = new ArrayList<>();
+        // 데이터 추가
+        list.add("강아지");
+        list.add("고양이");
+        list.add("코알라");
+        list.add("꿩");
+        // 데이터 조회
+        System.out.println(list.get(0));
+        // 인덱스 기준 데이터 삭제 출력이 되고 삭제됨
+        System.out.println(list.remove(2));
+        // 데이터 조회 후 삭제 삭제되었으면 true, 실패면 false
+        System.out.println(list.remove("고양이2"));
+        // 리스트 크기 출력
+        System.out.println(list.size());
+        // 리스트 전체 순회해서 출력
+        for(String i : list) {
+            System.out.println(i);
+        }
+
+
+
+    }
+}
--- src/chap_10/_03_Lambda.java
+++ src/chap_10/_03_Lambda.java
@@ -5,18 +5,22 @@ public class _03_Lambda {
         // 람다식
         //(전달값1, 전달값2, 전달값3..) => {코드}
     }
+    // 기본식
 //    public void print() {
 //        String s = "test";
 //        System.out.println(s);
 //    }
+//    접근제한자,리턴자료형, 메서드명 생략가능
 //    () -> {
 //        String s = "test";
 //        System.out.println(s);
 //    }
+//    코드가 한줄일 경우 중괄호도 생략가능
 //    (s) -> System.out.println(s)
 //    public int add(int x, int y){
 //        return x + y;
 //    }
+//    접근제한자 반환값자료형, 메서드명 생략, 중괄호도 생략
 //    (x, y) -> x + y;
 
 
