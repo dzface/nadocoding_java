2dcceb08256f779eb0b6673b1ff527e04826359c
dzface <dzface031@gmail.com>
2024-10-20 23:30:51 +0900

    Stream 복습

--- src/chap_10/_05_Stream.java
+++ src/chap_10/_05_Stream.java
@@ -10,13 +10,18 @@ public class _05_Stream {
     public static void main(String[] args) {
         // 스트림 : 흐른다?
         int[] scores = {100, 95, 90 ,85, 80};
-
+        // Arrays.stream 으로 스트림 생성
         IntStream scoreStream = Arrays.stream(scores);
-
         String[] langs = {"python", "java", "javascripy", "C"};
-        Stream<String> stream = Arrays.stream(langs);
+        Stream<String> langStream = Arrays.stream(langs);
+
+        // Collection.stream()
         List<String> langList = new ArrayList<>();
         langList = Arrays.asList(langs);
         System.out.println(langList.size());
+
+        Stream<String> langListStream = langList.stream();
+        // 스트림 생성방법 2 Stream.of()
+        Stream<String> langListOfStream = Stream.of(langs);
     }
 }
