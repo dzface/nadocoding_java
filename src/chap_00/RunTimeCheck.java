d2bd33ff311e3eb8a5374f54411d0a5aa4cfe434
dzface <dzface031@gmail.com>
2024-10-22 14:06:46 +0900

    System.nano()로 나노초 단위 수행시간 측정

--- src/chap_00/RunTimeCheck.java
+++ src/chap_00/RunTimeCheck.java
@@ -6,14 +6,14 @@ import java.util.Arrays;
 public class RunTimeCheck {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
-        long startTime = System.currentTimeMillis(); // 코드 시작시간
+        long startTime = System.nanoTime(); // 코드 시작시간
         for (int i = 0; i < 10000; i++) {
             list.add(i);
             System.out.println(list.get(i));
         }
-        long endTime = System.currentTimeMillis();
+        long endTime = System.nanoTime();
         long runningTime = endTime - startTime;
-        System.out.println("실행시간 : " + runningTime + "m/s");
-        System.out.println("실행시간 : " + (runningTime/1000) + "sec");
+        System.out.println("실행시간 : " + runningTime + "n/s");
+        System.out.println("실행시간 : " + (runningTime/1000000000) + "sec");
     }
 }
