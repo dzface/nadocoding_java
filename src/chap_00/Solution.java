569200d2b49678a0b2b8f12125a844f9bc8958a6
dzface <dzface031@gmail.com>
2024-10-25 00:52:09 +0900

    프로그래머스 배열만들기2 풀이 미해결

--- src/chap_00/Solution.java
+++ src/chap_00/Solution.java
@@ -1,13 +1,14 @@
 package chap_00;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Solution {
     public static void main(String[] args) {
         int l = 5;
-        int r= 5550;
+        int r= 555;
 
         String sl = String.valueOf(l);
         String sr = String.valueOf(r);
@@ -18,7 +19,23 @@ public class Solution {
         List<String> list = new ArrayList<>();
         list.add(first);
         addString(list, first,sr.length()-sl.length());
-        System.out.println(list);
+
+        for(String i: list) {
+            System.out.println(i);
+        }
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < answer.length; i++) {
+            if(Integer.parseInt(list.get(i))>=l && Integer.parseInt(list.get(i))<=r){
+                answer[i] = Integer.parseInt(list.get(i));
+            }
+        }
+        Arrays.sort(answer);
+        if(answer[list.size()-1]==0){
+            answer[list.size()-1] =-1;
+        }
+        for(int i: answer) {
+            System.out.println(i);
+        }
     }
 
     public static List<String> addString(List<String> list, String s, int limit) {
